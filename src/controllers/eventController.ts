9bc48f187c2ab2006b2f34aaaead3ddb16f68b37
ronald <ronald881996@gmail.com>
2025-02-15 12:56:49 +0100

    test: remove test controller for now

--- src/controllers/eventController.ts
+++ src/controllers/eventController.ts
@@ -6,9 +6,10 @@ const eventService = new EventService();
 export const addEvent = async (req: Request, res: Response): Promise<Response> => {
 	try {
 		const { itemId, description, location, custodian } = req.body;
+		console.log('llego');
 
 		const newEvent = await eventService.addEvent(description, location, custodian, itemId);
-
+		console.log('paso', newEvent);
 		return res.status(201).json(newEvent);
 	} catch (error) {
 		return res.status(500).json({ message: 'Error adding event', error });
