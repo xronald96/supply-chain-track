06aa0b8239f0f8458c344d3d6be4248a8a88a4d8
ronald <ronald881996@gmail.com>
2025-02-15 12:59:03 +0100

    refacrtor: adding missing validations

--- src/routes/item.ts
+++ src/routes/item.ts
@@ -4,11 +4,23 @@ import {
 	handleValidationErrors,
 	validateCreateItem,
 	validateUpdateItem,
+	validateItemEvents,
+	validateLastEvent,
 } from './validators/itemValidator';
 
 export const itemRouter = Router();
 
 itemRouter.post('/', ...validateCreateItem, handleValidationErrors as any, createItem);
 itemRouter.put('/:id', validateUpdateItem, handleValidationErrors as any, updateItem as any);
-itemRouter.get('/:id/events', getItemEvents as any);
-itemRouter.get('/:itemId/last-event', getLastEvent as any);
+itemRouter.get(
+	'/:id/events',
+	...validateItemEvents,
+	handleValidationErrors as any,
+	getItemEvents as any,
+);
+itemRouter.get(
+	'/:itemId/last-event',
+	...validateLastEvent,
+	handleValidationErrors as any,
+	getLastEvent as any,
+);
